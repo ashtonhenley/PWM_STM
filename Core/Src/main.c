7990d0a4d10e190331df5e39437f1a9e7c133806
Ashton Henley <ashton.henley@ametek.com>
2025-07-17 09:23:15 -0400

    Not working

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -70,6 +70,7 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
+	TIM1->CCR1 = 16383; // 0.25% of 65535, 25% duty cycle
 
   /* USER CODE END 1 */
 
@@ -121,6 +122,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
 
   }
   /* USER CODE END 3 */
@@ -188,7 +190,7 @@ static void MX_TIM1_Init(void)
   htim1.Init.Period = 65535;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
-  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
   {
     Error_Handler();
